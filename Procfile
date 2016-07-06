5647e5dfb6894734b965c0dc86513a54d407f9bd
Samaksh Jain <samakshjain@live.com>
2016-07-07 00:54:32 +0530

    updated to flask 0.11~

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:bluhrg
+web: gunicorn app:app
--- app.py
+++ app.py
@@ -5,42 +5,71 @@
 # @Last modified time: Wednesday, July 6th 2016, 9:35:16 pm(IST)
 # @License: MIT
 
-
-import sqlite3, os
-from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash, send_from_directory
 from contextlib import closing
 
 # Flask config
-bluhrg = Flask(__name__)
-bluhrg.config.from_object(os.environ['APP_SETTINGS'])
-
-# init db
-def init_db():
-    with closing(connect_db()) as db:
-        with bluhrg.open_resource('db.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 # connect to db
 def connect_db():
-    return sqlite3.connect(bluhrg.config['DATABASE'])
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
 
-# connect to db before each request
-@bluhrg.before_request
-def before_request():
-    g.db = connect_db()
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
 
-# disconnect when done (even when exceptions)
-@bluhrg.teardown_request
-def teardown_request(exception):
-    db = getattr(g, 'db', None)
-    if db is not None:
-        db.close()
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 
-@bluhrg.route('/')
+# init db
+def init_db():
+    db = get_db()
+    with app.open_resource('db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+# adding commnad to initdb
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print 'Initialized the database.'
+
+@app.route('/')
 def index():
-    return "<h3>Hallo! Let's start the bluhrg!</h3>"
+    db = get_db()
+    cur = db.execute('select heading, content from blag order by id desc')
+    entries = [dict(heading=row[0], content=row[1]) for row in cur.fetchall()]
+    return render_template('main.html', entries=entries)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/add', methods=['POST'])
+def bluhrg_post():
+    db = get_db()
+    db.execute('insert into blag (heading, content) values (?, ?)',
+                 [request.form['heading'], request.form['content']])
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
-    bluhrg.run()
+    app.run()
