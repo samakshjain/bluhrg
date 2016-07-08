50598839de668e672f3862d9e7b22fc62a79b982
Samaksh Jain <samakshjain@live.com>
2016-07-08 22:49:41 +0530

    added admin panel and initial pagination

--- app.py
+++ app.py
@@ -6,7 +6,7 @@
 # @License: MIT
 
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash, send_from_directory
+     render_template, flash, send_from_directory, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
 
@@ -19,6 +19,7 @@ db = SQLAlchemy(app)
 from models import *
 
 @app.route('/')
+@app.route('/<int:id>')
 def index():
     entries = BluhrgPost.query.all()
     return render_template('main.html', entries=entries)
@@ -36,6 +37,17 @@ def bluhrg_post():
     db.session.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('index'))
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+@app.route('/metadata')
+def get_metadata():
+    entries = BluhrgPost.query.all()
+    total = len(entries)
+    return jsonify(total_posts=total)
+
 
 if __name__ == "__main__":
     app.run()
